905e944319c6bbe98b8ed6ab45aec491baa75b28
Aguinaldo <parreirajr2@hotmail.com>
2022-06-08 02:01:46 -0300

    projeto MA finalizado

--- Projeto_MA/APP/Piscina_IOT/paginas/Historico.js
+++ Projeto_MA/APP/Piscina_IOT/paginas/Historico.js
@@ -1,11 +1,125 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react'
 import { StyleSheet, Text, View,Image,TouchableOpacity,ScrollView } from 'react-native';
 import { CommonActions, useNavigation } from '@react-navigation/native';
+import {LineChart} from "react-native-chart-kit";
 const river = require('../imagens/river.png');
+const Influxdb = require("influxdb-v2");
+
+
+
+
+
+async function getData() {
+    const influxdb = new Influxdb({
+     // host: "8a7708eb18e6.sn.mynetname.net",
+     // port: 5000,
+      host: "20.210.132.42",
+      port: 8086,
+      protocol: "http",
+      token:
+        "ynCP2gPa4OT16BfIRrE7Jq4uGMu1LrFmgaPJgCKZIF_nO7n3Jewa-zlerPm69f-ZKWrZYIr63XoXbDhfPasWpw==",
+    });
+  
+    const teste = await influxdb.query(
+      { org: "7e377cc6cc59a03f" },
+      {
+        query:
+         //'from(bucket: "press") |> range(start: -30d) |> filter(fn: (r) => r["_measurement"] == "mqtt_consumer"  and r["_field"] == "Pressao" and r["topic"] == "/teste/smartpress/'+mac+'/payload")'
+            'from(bucket: "Aguinaldo_IOT")|> range(start: -30m)|> filter(fn: (r) => r["_measurement"] == "objects" or r["_measurement"] == "uptimesec" or r["_measurement"] == "distance")|> filter(fn: (r) => r["device"] == "tasmota_704AE0")|> filter(fn: (r) => r["sensor"] == "sr04")'
+        //-30m
+        }
+    );
+
+    let data = {
+        nivel: [],
+        instantes:[],
+        instantes_formatado:[],
+    }
+
+    for(let i = 0; i < teste[0].length; i++){
+        data.nivel.push(2,36-Number(teste[0][i]._value))
+        data.instantes.push(Date(teste[0][i]._time))
+    }
+
+      data.instantes.map(item => {
+          var date = new Date(item);
+          var str_dmy = date.toLocaleDateString('en-GB');
+          var str_hms = date.toLocaleTimeString('pt-BR');
+          var str_hms_dmy = str_hms + "\n\n" + str_dmy;
+          data.instantes_formatado.push(str_hms);
+          //data.instantes_formatado.push(str_hms_dmy);
+      })
+      
+
+    return data ;
+  
+  }
+
+  
+
+
+
 
 export default function Historico(){
 
     const navigation = useNavigation();
+
+    const [influx_valores,Set_influx_valores] = useState([0]);
+    const [influx_times,Set_influx_times] = useState([]);
+
+
+
+
+    const data = {
+        labels: influx_times,
+        datasets: [
+          {
+            data: influx_valores,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // optional
+            strokeWidth: 2 // optional
+          },
+
+         
+
+          
+        ],
+        legend: ["Nivel (cm)"] // optional
+      };
+
+
+
+      const chartConfig = {
+        backgroundColor: 'green',
+        backgroundGradientFrom: "#63A46C",
+        backgroundGradientFromOpacity: 1,
+        backgroundGradientTo: "#56865D",
+        backgroundGradientToOpacity: 1,
+        color: (opacity = 0.5) => `rgba(0, 0, 0, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        strokeWidth: 3, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: true, // optional
+        
+        
+      };
+
+      useEffect(()=>{
+
+            setTimeout(function() {
+                getData().then((r) => {
+                    
+                  var nivel_5 = r['nivel'].slice(-5);
+                  var instantes_5 = r['instantes_formatado'].slice(-5);          
+                  Set_influx_valores(nivel_5);
+                  Set_influx_times(instantes_5);
+        
+                  })
+            }, 10000);
+
+        });
+
+
+
 
     return(
 
@@ -18,22 +132,31 @@ export default function Historico(){
             
             <View>
                 <Text style = {styles.txt_title2}>Selecione o local</Text>
-                <Text style = {styles.label_green}>dd/mm/aaaa</Text>
-            </View>
-
-            <View>
-                <Text style = {styles.txt_title2}>Selecione a data</Text>
                 <Text style = {styles.label_green}>Canal 3 - Comporta Intermediária</Text>
             </View>
 
             <View>
-                <Text style = {styles.txt_title2}>  Data                          Hora         Nivel</Text>
-                <View style = {styles.center}>
-                    <Text style = {styles.label_green_lg}>26/05/2022                      12:45            80CM</Text>
-                    <Text style = {styles.label_green_lg}>26/05/2022                      12:55            80CM</Text>
-                    <Text style = {styles.label_green_lg}>26/05/2022                      13:05            80CM</Text>
-                    <Text style = {styles.label_green_lg}>26/05/2022                      13:15            80CM</Text>
-                </View>
+                <Text style = {styles.txt_title2}>Selecione a data</Text>
+                <Text style = {styles.label_green}>dd/mm/aaaa</Text>
+            </View>
+
+            <View style={styles.grafic}>
+                
+                    <LineChart
+                        data={data}
+                        width={350}
+                        height={220}
+                        chartConfig={chartConfig}
+                        fromZero={true}
+                        style = {{
+                                borderRadius: 16,
+                                
+                            }
+                        }
+
+                        
+                    />
+                
             </View>
             
             
@@ -92,6 +215,12 @@ const styles = StyleSheet.create({
         borderBottomWidth:1,
         
     },
+    grafic:{
+
+        marginTop:20,
+        alignItems:'center',
+        justifyContent:'center',
+    }
  
 
 
--- Projeto_MA/APP/Piscina_IOT/paginas/Mediçao.js
+++ Projeto_MA/APP/Piscina_IOT/paginas/Mediçao.js
@@ -1,11 +1,83 @@
-import React from 'react';
-import { StyleSheet, Text, View,Image,TouchableOpacity,ScrollView } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { StyleSheet, Text, View,Image,TouchableOpacity,ScrollView, Alert } from 'react-native';
 import { CommonActions, useNavigation } from '@react-navigation/native';
+const Influxdb = require("influxdb-v2");
 const river = require('../imagens/river.png');
+
+
+
+
+async function getData() {
+    const influxdb = new Influxdb({
+     // host: "8a7708eb18e6.sn.mynetname.net",
+     // port: 5000,
+      host: "20.210.132.42",
+      port: 8086,
+      protocol: "http",
+      token:
+        "ynCP2gPa4OT16BfIRrE7Jq4uGMu1LrFmgaPJgCKZIF_nO7n3Jewa-zlerPm69f-ZKWrZYIr63XoXbDhfPasWpw==",
+    });
+  
+    const teste = await influxdb.query(
+      { org: "7e377cc6cc59a03f" },
+      {
+        query:
+         //'from(bucket: "press") |> range(start: -30d) |> filter(fn: (r) => r["_measurement"] == "mqtt_consumer"  and r["_field"] == "Pressao" and r["topic"] == "/teste/smartpress/'+mac+'/payload")'
+            'from(bucket: "Aguinaldo_IOT")|> range(start: -10s)|> filter(fn: (r) => r["_measurement"] == "objects" or r["_measurement"] == "uptimesec" or r["_measurement"] == "distance")|> filter(fn: (r) => r["device"] == "tasmota_704AE0")|> filter(fn: (r) => r["sensor"] == "sr04")'
+        //-10s
+        }
+    );
+
+    let nivel = 0;
+
+    
+    console.log('hello');
+    nivel = teste[0][0]._value;
+    console.log(teste[0][0]._value);
+
+      
+      
+
+    return nivel ;
+  
+  }
+
+
+
 
 export default function Mediçao(){
 
     const navigation = useNavigation();
+
+    const [influx_valor,Set_influx_valor] = useState();
+    const [status,Set_status] = useState();
+
+
+    useEffect(()=>{
+
+        setTimeout(function() {
+            getData().then((r) => {
+                
+              var nivel = r;
+              nivel = 2,36 - nivel;
+              console.log(r);
+              Set_influx_valor(nivel.toFixed(0));
+              if(nivel>0 && nivel<=120){
+                  Set_status("NORMAL");
+                  //alert("ABRIR COMPORTA");
+              }
+              else if (nivel>120 && nivel<=180){
+                  Set_status("ALERTA");
+              }
+              else if (nivel>180){
+                  Set_status("ALERTA MAXIMO");
+                  alert("ABRIR COMPORTA");
+              }
+    
+              })
+        }, 10000);
+
+    });
 
     return(
 
@@ -23,7 +95,12 @@ export default function Mediçao(){
 
             <View>
                 <Text style = {styles.txt_title2}>Nivel de agua</Text>
-                <View style = {styles.center}><Text style = {styles.label_green_lg}>70 cm</Text></View>
+                <View style = {styles.center}><Text style = {styles.label_green_lg}>{influx_valor} cm</Text></View>
+            </View>
+
+            <View>
+                <Text style = {styles.txt_title2}>Status</Text>
+                <Text style = {styles.label_green}>{status}</Text>
             </View>
 
 
@@ -35,7 +112,7 @@ export default function Mediçao(){
 
             <View>
                 <Text style = {styles.txt_title2}>Nivel de agua</Text>
-                <View style = {styles.center}><Text style = {styles.label_green_lg}>70 cm</Text></View>
+                <View style = {styles.center}><Text style = {styles.label_green_lg}>{influx_valor} cm</Text></View>
             </View>
             
             <View>
